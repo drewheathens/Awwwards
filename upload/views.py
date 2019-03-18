050dcbb2195ddf6d219e8a43416719f35289c472
drewheathens <mudavadie@gmail.com>
2019-03-18 11:53:02 +0300

    modified views

--- upload/views.py
+++ upload/views.py
@@ -4,3 +4,30 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def search_results(request):
+	if 'profile' in request.GET and request.GET["user"]:
+        search_term = request.GET.get("user")
+        searched_users = Profile.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"users": searched_users})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+
+@login_required(login_url='/accounts/login/')
+def edit_profile(request):
+    current_user=request.user
+    if request.method =='POST':
+        form=ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            profile=form.save(commit=False)
+            profile.username = current_user
+            profile.save()
+
+    else:
+        form=ProfileForm()
+
+    return render(request,'editProfile.html',{"form":form})
