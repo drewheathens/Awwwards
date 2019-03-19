159cc562a8858f360ba63161fceff868c750168a
drewheathens <mudavadie@gmail.com>
2019-03-19 07:32:31 +0300

    modified admin

--- upload/admin.py
+++ upload/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Profile,Post
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Post)
