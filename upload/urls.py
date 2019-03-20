0826d7e6dda14f5fdcd6a5a23fd767fdfaddecb7
drewheathens <mudavadie@gmail.com>
2019-03-20 19:06:10 +0300

    added vote file

--- upload/urls.py
+++ upload/urls.py
@@ -8,7 +8,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^$',views.index,name='indexPage'),
     url(r'^projects/(\d+)',views.project,name ='projects'),
-    url(r'^new/project$', views.new_project, name='newProject'),
+    url(r'^new/project$', views.new_project, name='new-project'),
     url(r'^profile/',views.profile,name = 'Profile'),
     url(r'^edit/profile/$',views.edit_profile,name = 'editProfile'),
     url(r'^search/', views.search_results, name='search_results'),
