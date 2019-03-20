b96cfbbbcec1096c1ac83f1b5f1668138954ba04
drewheathens <mudavadie@gmail.com>
2019-03-20 14:06:54 +0300

    modified req

--- upload/urls.py
+++ upload/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 
 
 urlpatterns=[
-    # url(r'^signup/',views.signup,name='signup'),
-    url(r'^$', views.signup, name='signup'),
+    url(r'^signup/',views.signup,name='signup'),
+    # url(r'^', views.signup, name='signup'),
     url(r'accounts/', include('django.contrib.auth.urls')),
     url(r'^search/',views.search_results,name='search_results'),
     url(r'^new/post$',views.new_post, name='newPost'),
